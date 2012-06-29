f337f3fb426053debfd45e4edc99083f06844bf0
Grant Rodgers <grantr@gmail.com>
2012-06-28 22:37:06 -0700

    remove gossip from node and node manager
    External state management should be handled by the registry.
    
    Also make nodes the responsibility of the node manager. They are no
    longer actors, only state machines and sockets.

--- lib/dcell/node.rb
+++ lib/dcell/node.rb
@@ -1,9 +1,9 @@
 module DCell
   # A node in a DCell cluster
   class Node
-    include Celluloid
     include Celluloid::FSM
-    attr_reader :id, :addr, :timestamp
+    include Celluloid::Notifications
+    attr_reader :id, :addr
 
     # FSM
     default_state :disconnected
@@ -16,28 +16,13 @@ module DCell
       Celluloid::Logger.warn "Communication with #{id} interrupted"
     end
 
-    # Singleton methods
-    class << self
-      include Enumerable
-      extend Forwardable
-
-      def_delegators "Celluloid::Actor[:node_manager]", :all, :each, :find, :[], :handle_gossip
-      def_delegators "Celluloid::Actor[:node_manager]", :gossip_rate, :heartbeat_timeout
-    end
-
     def initialize(id, addr)
-      @id, @addr = id, addr
-      @timestamp = 0
-      @socket = nil
-      @fresh = true
-
-      # Total hax to accommodate the new Celluloid::FSM API
-      attach self
+      @id = id
+      @addr = addr
     end
 
     def finalize
       transition :shutdown
-      @gossip.cancel if @gossip
       @socket.close if @socket
     end
 
@@ -54,37 +39,8 @@ module DCell
         raise
       end
 
-      transition :connected
       @socket
     end
-
-    # Find an actor registered with a given name on this node
-    def find(name)
-      request = Message::Find.new(Thread.mailbox, name)
-      send_message request
-
-      response = receive do |msg|
-        msg.respond_to?(:request_id) && msg.request_id == request.id
-      end
-
-      abort response.value if response.is_a? ErrorResponse
-      response.value
-    end
-    alias_method :[], :find
-
-    # List all registered actors on this node
-    def actors
-      request = Message::List.new(Thread.mailbox)
-      send_message request
-
-      response = receive do |msg|
-        msg.respond_to?(:request_id) && msg.request_id == request.id
-      end
-
-      abort response.value if response.is_a? ErrorResponse
-      response.value
-    end
-    alias_method :all, :actors
 
     # Send a message to another DCell node
     def send_message(message)
@@ -97,27 +53,6 @@ module DCell
       socket << message
     end
     alias_method :<<, :send_message
-
-    def tick
-      @timestamp += 1
-    end
-
-    def fresh?
-      @fresh
-    end
-
-    # Handle an incoming timestamp observation for this node
-    def handle_timestamp(t)
-      @fresh = false if t > 0
-      if @timestamp < t
-        @timestamp = t
-        transition :connected
-        transition :partitioned, :delay => self.class.heartbeat_timeout
-        unless state == :connected
-          Celluloid::Logger.info "Revived node #{id}"
-        end
-      end
-    end
 
     # Friendlier inspection
     def inspect
--- lib/dcell/node_manager.rb
+++ lib/dcell/node_manager.rb
@@ -1,81 +1,22 @@
 module DCell
   # Manage nodes we're connected to
+  # Nodes are a state machine and a socket
+  # The directory calls methods to add and remove nodes
   class NodeManager
     include Celluloid::ZMQ
-    include Enumerable
 
-    attr_reader :gossip_rate, :heartbeat_timeout
-
-    def initialize
-      @nodes = {}
-
-      @gossip_rate       = 5  # How often to send gossip in seconds
-      @heartbeat_timeout = 10 # How soon until a lost heartbeat triggers a node partition
-      each { |node| node.socket if node } # Connect all so we can gossip
-      @gossip = after(gossip_rate) { gossip_timeout }
+    def nodes
+      @nodes ||= {}
     end
 
-    # Return all available nodes in the cluster
-    def all
-      Directory.all.map do |node_id|
-        find node_id
+    def add_node(id, addr)
+      @nodes[id] = Node.new(id, addr).tap do |node|
+        node.attach self
       end
     end
 
-    # Iterate across all available nodes
-    def each
-      Directory.all.each do |node_id|
-        yield find node_id
-      end
-    end
-
-    # Find a node by its node ID
-    def find(id)
-      node = @nodes[id]
-      return node if node
-
-      addr = Directory[id]
-      return unless addr
-
-      if id == DCell.id
-        node = DCell.me
-      else
-        node = Node.new(id, addr)
-      end
-
-      @nodes[id] ||= node
-      @nodes[id]
-    end
-    alias_method :[], :find
-
-    # Send gossip to a random node (except ourself) after the given interval
-    def gossip_timeout
-      nodes = select { |node| node.state == :connected }
-      peer = nodes.select { |node| node.id != DCell.id }.sample(1)[0]
-      if peer
-        nodes = nodes.inject([]) { |a,n| a << [n.id, n.addr, n.timestamp]; a }
-        data = nil
-        if DCell.registry.is_a? Registry::GossipAdapter
-          data = peer.fresh? ? DCell.registry.values : DCell.registry.changed
-        end
-        DCell.me.tick
-        peer.send_message DCell::Message::Gossip.new nodes, data
-      end
-      @gossip = after(gossip_rate) { gossip_timeout }
-    end
-
-    def handle_gossip(peers, data)
-      peers.each do |id, addr, timestamp|
-        if (node = find(id))
-          node.handle_timestamp! timestamp
-        else
-          Directory[id] = addr
-          Celluloid::Logger.info "Found node #{id}"
-        end
-      end
-      if DCell.registry.is_a? Registry::GossipAdapter
-        data.map { |data| DCell.registry.observe data } if data
-      end
+    def del_node(id)
+      @nodes.delete(id)
     end
   end
 end
