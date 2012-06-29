397f0120706302b10de258d3c1be459e6c609fe5
Grant Rodgers <grantr@gmail.com>
2012-06-28 23:29:57 -0700

    subscribe to registry notifications
    Create and delete nodes in the NodeManager as the registry publishes
    state changes.

--- lib/dcell/node_manager.rb
+++ lib/dcell/node_manager.rb
@@ -4,19 +4,35 @@ module DCell
   # The directory calls methods to add and remove nodes
   class NodeManager
     include Celluloid::ZMQ
+    include Celluloid::Notifications
+
+    def initialize
+      #TODO get current nodes from registry
+      subscribe(/^registry.node/, :handle_registry)
+    end
+
+    def handle_registry(topic, *args)
+      puts "handling from registry #{topic} #{args.inspect}"
+      case topic
+      when "registry.node.set"
+        add_node(*args)
+      when "registry.node.del"
+        del_node(*args)
+      end
+    end
 
     def nodes
       @nodes ||= {}
     end
 
     def add_node(id, addr)
-      @nodes[id] = Node.new(id, addr).tap do |node|
+      nodes[id] = Node.new(id, addr).tap do |node|
         node.attach self
       end
     end
 
     def del_node(id)
-      @nodes.delete(id)
+      nodes.delete(id)
     end
   end
 end
