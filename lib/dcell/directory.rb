d1100ff9745daf0c84842ecde688249250170651
Grant Rodgers <grantr@gmail.com>
2012-06-28 22:38:09 -0700

    make directory and global actors
    Directory and Global are the internal interfaces to external state. They
    subscribe to notifications from nodes and the registry.

--- lib/dcell/directory.rb
+++ lib/dcell/directory.rb
@@ -1,35 +1,49 @@
 module DCell
   # Directory of nodes connected to the DCell cluster
-  module Directory
-    extend self
+  # The directory subscribes to notifications from the registry about nodes
+  # being added or removed
+  # The directory implements node find and enumeration methods
+  class Directory
+    include Celluloid
+    include Celluloid::Notifications
 
-    @@directory = {}
-    @@directory_lock = Mutex.new
+    class << self
+      include Enumerable
+      extend Forwardable
 
-    # Get the URL for a particular Node ID
+      def_delegators "Celluloid::Actor[:directory]", :all, :each, :find
+    end
+
+    def initialize
+      @directory = {}
+    end
+
+    # Get the Node for a particular Node ID
     def get(node_id)
-      @@directory_lock.synchronize do
-        @@directory[node_id]
-      end
+      @directory[node_id]
     end
     alias_method :[], :get
 
-    # Set the address of a particular Node ID
-    def set(node_id, addr)
-      @@directory_lock.synchronize do
-        @@directory[node_id] = addr
-      end
+    # Set the Node for a particular Node ID
+    def set(node_id, node)
+      @directory[node_id] = node
     end
     alias_method :[]=, :set
 
-    # List all of the node IDs in the directory
+    # List all of the nodes in the directory
     def all
-      @@directory_lock.synchronize { @@directory.keys }
+      @directory.values
+    end
+
+    def each
+      @directory.each do |node_id, node|
+        yield node
+      end
     end
 
     # Clear the directory.
     def clear
-      @@directory_lock.synchronize { @@directory.clear }
+      @directory.clear
     end
   end
 end
--- lib/dcell/global.rb
+++ lib/dcell/global.rb
@@ -1,23 +1,35 @@
 module DCell
   # Global object registry shared among all DCell nodes
-  module Global
-    extend self
+  class Global
+    include Celluloid
+    include Celluloid::Notifications
+
+    class << self
+      include Enumerable
+      extend Forwardable
+
+      def_delegators "Celluloid::Actor[:global]", :all, :each, :find
+    end
+
+    def initialize
+      @globals = {}
+    end
 
     # Get a global value
     def get(key)
-      DCell.registry.get_global key.to_s
+      @globals[key]
     end
     alias_method :[], :get
 
     # Set a global value
     def set(key, value)
-      DCell.registry.set_global key.to_s, value
+      @globals[key] = value
     end
     alias_method :[]=, :set
 
     # Get the keys for all the globals in the system
     def keys
-      DCell.registry.global_keys.map(&:to_sym)
+      @globals.keys
     end
   end
 end
